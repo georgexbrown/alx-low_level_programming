4a8ec092dbc731221c20846f5c6b46de017d75e2
George Osondu <browngeorge752@gmail.com>
2022-09-14 03:54:17 +0000

    Timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - Printing multiplication table for 9
+ *
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = b * a;
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
