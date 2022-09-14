5e700ecb014c28dc318fe3f9d9443736014f159c
George Osondu <browngeorge752@gmail.com>
2022-09-14 00:02:11 +0000

    Ten times of alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@ void print_alphabet_x10(void)
 	int ch, i;
 
 	i = 1;
-	ch = 'a';
-	while (i < 11)
+	while (i <= 10)
 	{
+		ch = 'a';
 		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
 		}
-		_putchar('\n');
 		i++;
+		_putchar('\n');
 	}
 }
