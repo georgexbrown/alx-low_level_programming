3a17577d93dd6f9320399bec2a27fc8596cd74fe
George Osondu <browngeorge752@gmail.com>
2022-09-14 04:10:21 +0000

    Finishing up

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * add - adds up two integers
+ * @a: int type parameter (summing with b)
+ * @b: int type parameter (summing with a)
+ * Return: sum of value of a and b
+ */
+
+int add(int a, int b)
+
+{
+
+	int c;
+
+
+
+	c = a + b;
+
+
+
+	return (c);
+
+}
