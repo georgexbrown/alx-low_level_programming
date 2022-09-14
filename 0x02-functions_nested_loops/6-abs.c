94871fcd5b7f67ad0363b15e4b7cc1d77b40b0fc
George Osondu <browngeorge752@gmail.com>
2022-09-14 01:33:22 +0000

    Absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,14 @@
 
 /**
  * _abs - Prints absolute number
- *@int: Parameter type
+ *@x: Parameter with type int
  * Description: Function that prints absolute number of a given numbers
  *
  * Return: Always 0 (Success)
  *
  */
-int _abs(int)
+int _abs(int x)
 {
-	abs();
+	abs(x);
 	return (0);
 }
