c80697047c86d39ae1ee67eca009f46644b61e80
George Osondu <browngeorge752@gmail.com>
2022-09-14 00:24:09 +0000

    Checking lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,19 @@
 
 /**
  * _islower - detects if an integer is lower and retuns value
+ * @c: parameter with int type
  *
  * Return: 1 if true and 0 if false
  *
  */
 int _islower(int c)
 {
-	if islower(c)
+	if (islower(c))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
