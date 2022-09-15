16966a2865f38996b4f20ecffeef9df43e8b6b30
George Osondu <browngeorge752@gmail.com>
2022-09-15 22:08:55 +0000

    Printing 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
