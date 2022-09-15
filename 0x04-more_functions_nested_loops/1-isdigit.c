5b674b1ec54fac1064e602784161de98993218e5
George Osondu <browngeorge752@gmail.com>
2022-09-15 21:31:06 +0000

    Checking if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - Checks if an entry is a digit and returns value
+ * 
+ * Return: 1 if digit while 0 if not
+ *
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
