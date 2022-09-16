cd59000f884a7c55880e986dfea6fd3cc24f027c
George Osondu <browngeorge752@gmail.com>
2022-09-16 01:53:39 +0000

    More practice

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,8 +8,7 @@ void more_numbers(void)
 {
 	int i, x;
 
-	i = 0;
-	while (i < 10)
+	for (i = 1; i < 11; i++)
 	{
 		x = 0;
 		while (x < 15)
@@ -17,7 +16,6 @@ void more_numbers(void)
 			_putchar(x + '0');
 			x++;
 		}
-		i++;
 		_putchar('\n');
 	}
 	_putchar('\n');
