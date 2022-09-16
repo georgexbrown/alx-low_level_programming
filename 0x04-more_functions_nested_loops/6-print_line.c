13816ab9308873b9de9d54b6eb5ada5c1b24fc50
George Osondu <browngeorge752@gmail.com>
2022-09-16 02:55:38 +0000

    Drawings 1&2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,7 +16,7 @@ void print_line(int n)
 		int i;
 
 		for (i = 1; i <= n; i++)
-		{	
+		{
 			_putchar('_');
 		}
 		_putchar('\n');
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line across the screen
+ *@n: integer parameter that returns no value
+ *
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
+
