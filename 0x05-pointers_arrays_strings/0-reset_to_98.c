44c275e89395d0fccdf960f8a6e73c6036406cf0
George Osondu <browngeorge752@gmail.com>
2022-09-19 20:44:37 +0000

    Resetting t0 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - function that changes the value of n from 402 to 98
- * @*n: integer parameter that returns no value
+ * @n: integer parameter that returns no value
  */
 void reset_to_98(int *n)
 {
