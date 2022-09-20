69ded1b785aa48165af3913f577fc62a98a0c1c5
George Osondu <browngeorge752@gmail.com>
2022-09-20 04:08:41 +0000

    String to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - Function that converts a string to integer
+ * @s: Character pointer parameter
+ *
+ * Return: an integer type
+ */
+int _atoi(char *s)
+{
+	int c;
+
+	c = atoi(s);
+	return (c);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -25,6 +25,8 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+int _atoi(char *s);
+
 
 
 #endif
