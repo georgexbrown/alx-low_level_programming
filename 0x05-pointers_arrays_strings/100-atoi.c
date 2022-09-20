85f02a7c28b2d92a928b953889d5a50543b44e89
George Osondu <browngeorge752@gmail.com>
2022-09-20 04:45:58 +0000

    String to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,6 @@ int _atoi(char *s)
 {
 	int c, i;
 
-	c = 0;
 	while (s[c] != '\0')
 	{
 		if (s[c] >= '1' && s[c] <= '9')
@@ -20,5 +19,5 @@ int _atoi(char *s)
 		}
 		c++;
 	}
-	return (c);
+	return (i);
 }
