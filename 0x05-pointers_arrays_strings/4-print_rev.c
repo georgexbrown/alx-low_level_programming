63f37f8550425a73bb1646a30e80c01f513e5895
George Osondu <browngeorge752@gmail.com>
2022-09-20 00:39:35 +0000

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - Function that prints a string in reverse
+ * @s: Character pointer parameter
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
