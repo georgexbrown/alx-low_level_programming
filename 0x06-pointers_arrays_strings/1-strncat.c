d99aa0ac9bcc66f873f71adf46946d2fcdfde757
George Osondu <browngeorge752@gmail.com>
2022-09-22 13:46:20 +0000

    Copying strings with count

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * _strncpy - Function that copies strings
+ * _strncat - Function that copies strings
  * @dest: character pointer parameter that copies string src
  * @src: character pointer parameter that copies string dest
  * @n: integer parameter that represent the length of string to be copied
  * Return: Pointer *dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	strncpy(dest, src);
+	strncat(dest, src, n);
 	return (dest);
 }
