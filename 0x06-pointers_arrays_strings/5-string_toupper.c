6f985011cbcd8e21ad0a8b28ed37b8f38abbe3b6
George Osondu <browngeorge752@gmail.com>
2022-09-22 21:43:54 +0000

    to Uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * string_toupper - Function that changes lowercase to uppercase
+ * @*: Character pointer variable parameter
+ * Return: char *
+ */
+char *string_toupper(char *)
+{
+	int up;
+
+	up = toupper(*);
+	return (up);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,4 +21,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 
 
+char *string_toupper(char *);
+
+
 #endif
