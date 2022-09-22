0bbe28c78b8620522d143d1a7845bec313455cd6
George Osondu <browngeorge752@gmail.com>
2022-09-22 20:06:30 +0000

    Reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,18 +6,21 @@
  * @n: Integer parameter that represents number of items in array
  *
  */
-void reverse_array(int *a, int n)
+void reverse(int *a, int n)
 {
-	int aux[5];
 	int i;
+	int aux[5];
 
 	for (i = 0; i < n; i++)
 	{
-		aux[n - 1 - i] = *a[i];
+		aux[n - 1 - i] = a[i];
 	}
+
 	for (i = 0; i < n; i++)
 	{
-		*a[i] = aux[i];
-		printf("%d ", *a[i]);
+		a[i] = aux[i];
+		_putchar(a[i] + '0');
+		_putchar(' ');
 	}
+	_putchar('\n');
 }
