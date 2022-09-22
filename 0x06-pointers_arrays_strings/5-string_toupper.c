eec1f7733848cf633dd7295fab3ed2d5440347c2
George Osondu <browngeorge752@gmail.com>
2022-09-22 23:47:51 +0000

    to Uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 
 /**
  * string_toupper - Function that changes lowercase to uppercase
- * @char *: Character pointer variable parameter
+ * @s *: Character pointer variable parameter
  * Return: char *
  */
 char *string_toupper(char *s)
