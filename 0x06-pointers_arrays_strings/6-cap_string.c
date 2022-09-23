ae8ce30acd261c08d98f884d99dca733ebeddbd0
George Osondu <browngeorge752@gmail.com>
2022-09-23 00:21:20 +0000

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - Function to capitalize strings
+ * @c: Character pointer parameter
+ * Returns: c character
+ */
+char *cap_string(char *c)
+{
+	int i = 0, j;
+	int separator[] = {32};
+
+	if ((*(c + i) >= 97) && (*c + i) <= 122)
+		*(c + i) = *(c + i) - 32;
+	i++;
+	while (*(c + i) != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (*(c + i) == separator[j])
+			{
+				if (*(c + (i + 1)) >= 97) && (*(c + (i + 1)) <= 122)
+					*(c + (i + 1)) = *(c + (i + 1)) - 32;
+				break;
+			}
+		}
+		i++;
+	}
+	return (c);
+}
