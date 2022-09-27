b007b1b48e894e6cf44818ee601607713f24ace6
George Osondu <browngeorge752@gmail.com>
2022-09-27 00:55:14 +0000

    Chess games

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,12 +6,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < sizeof(a); i++)
+	for (unsigned int i = 0; i < sizeof(a); i++)
 	{
-		for (int j = 0; j < sizeof(a); j++)
+		for (unsigned int j = 0; j < sizeof(a); j++)
 		{
 			_putchar(a[i][j]);
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
