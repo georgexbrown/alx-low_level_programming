14d6abe548566835847c19ec014964811182ff6d
George Osondu <browngeorge752@gmail.com>
2022-09-28 23:29:16 +0000

    Reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Functions that reverses a string in recursion
+ * @s: Character pointer that holds the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,10 @@
 
 void _puts_recursion(char *s);
 
+void _print_rev_recursion(char *s);
+
+int _putchar(char c);
+
 
 
 #endif
