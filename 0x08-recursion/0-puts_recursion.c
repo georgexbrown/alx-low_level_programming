b514dc81c253cb2fb1f3353882972f0d7904a0c4
George Osondu <browngeorge752@gmail.com>
2022-09-28 19:50:44 +0000

    Printing strings

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Function that prints a string followed by a new line
+ * @s: character pointer parameter that hold the string
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+Deep Dive in C Programming
+The concept of Recursions in C
