ac0980a2802cd3423d85b722524ab23fb4ac0576
George Osondu <browngeorge752@gmail.com>
2022-09-29 02:30:41 +0000

    Base and Power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,5 +13,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	return (-x * _pow_recursion(x, (y - 1)));
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, (y - 1)));
 }
