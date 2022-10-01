5fc24b958e7684e6ce85a95cdae48a75be37c7e8
George Osondu <browngeorge752@gmail.com>
2022-10-01 01:08:02 +0000

    Argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,15 +7,8 @@
  * @argv: Argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	char *list;
-
 	printf("%d\n", argc);
-	if (argc == 500)
-	{
-		list = argv[argc];
-		printf("I dun tire = %s\n", list);
-	}
 	return (0);
 }
