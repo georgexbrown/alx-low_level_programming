c7cf2303a31ace8cdf48bdd793cbfb0e101971fd
George Osondu <browngeorge752@gmail.com>
2022-10-26 03:53:02 +0000

    adding from behind

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - a function that adds a new node at the end
+ * of a listint_t list
+ * @head: pointer to the head node of the struct
+ * @n: new element passed to the list
+ * Return: the address of the new element or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *endNode = NULL;
+	listint_t *tmp = NULL;
+
+	if (!head)
+		return (NULL);
+
+	endNode = malloc(sizeof(listint_t));
+	if (!(endNode))
+		return (NULL);
+	endNode->n = n;
+	endNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = endNode;
+		return (*head);
+	}
+
+	temp = *head;
+
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = endNode;
+	return (temp->next);
+}
