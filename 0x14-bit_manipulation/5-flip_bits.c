d8e2a6224d629fa9c7cf6c31baec9926e7202edd
George Osondu <browngeorge752@gmail.com>
2022-10-29 03:08:20 +0000

    dealing with bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int num;
 
-	for (num = 0; n || m; n >> 1, m >> 1)
+	for (num = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			num++;
