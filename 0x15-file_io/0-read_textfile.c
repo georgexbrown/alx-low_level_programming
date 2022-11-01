a5a12e0275353e7f58c3d1721ea229000145247f
George Osondu <browngeorge752@gmail.com>
2022-11-01 02:50:03 +0000

    Read & Print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints it to
+ * the POSIX standard output
+ * @filename: name of file to be read and printed
+ * @letters: number of letters to be read and printed
+ * Return: number of letters read and printed or 0 if filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t sysr, sysw;
+	ssize_t *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	sysr = read(fd, buffer, letters);
+	if (sysr == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	sysw = write(STDOUT_FILENO, buffer, sysr);
+	if (sysw != sysr)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (sysw);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,13 @@
+File Descriptors in C 
+
+----------------------
+
+Understanding the concept of File I/O System Calls and how to use Flags
+
+0 - a function that reads a text file and prints it to the POSIX standard output.
+
+1 - a function that creates a file.
+
+2 - a function that appends text at the end of a file.
+
+3 - a program that copies the content of a file to another file.
