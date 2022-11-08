a785376590178e967f304754043bdf5b7c8a6e0f
George Osondu <browngeorge752@gmail.com>
2022-11-08 02:39:28 +0000

    On startup

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void startUp(void)__attribute__((constructor));
 
 void startUp(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
