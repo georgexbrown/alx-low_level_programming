b537b7e4bf7442cebf4a3ded8e021f92cb67091a
George Osondu <browngeorge752@gmail.com>
2022-11-09 01:34:23 +0000

    Copy File

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,88 @@
+#include "main.h"
+
+/**
+ * _close - function that closes file descriptors
+ * @fd: file descriptor to be closed
+ * Return: Success
+ */
+int _close(const int fd)
+{
+	int exitFD;
+
+	exitFD = close(fd);
+	if (exitFD == -1)
+	{
+		printf("Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
+ * copy_file - function that copies the content of a file to another file
+ * @filefrom: file to be copied from
+ * @fileto: file to be copied to
+ * Return: Always 0 (Success)
+ */
+int copy_file(const char *filefrom, const char *fileto)
+{
+	char buffer[1024];
+	int firstFD, secondFD;
+	ssize_t sysRead, sysWrite;
+
+	if (filefrom == NULL)
+	{
+		printf("Error: Can't read from file, %s\n", filefrom);
+		exit(98);
+	}
+	firstFD = open(filefrom, O_RDONLY);
+	if (firstFD == -1)
+	{
+		printf("Error: Can't read from file, %s\n", filefrom);
+		exit(98);
+	}
+	
+	secondFD = open(fileto, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (secondFD == -1)
+	{
+		printf("Error: Can't write to, %s\n", fileto);
+		exit(99);
+	}
+
+	sysRead = read(firstFD, buffer, 1024);
+	if (sysRead == -1)
+	{
+		printf("Error: Can't read from file, %s\n", filefrom);
+		_close(firstFD);
+		exit(98);
+	}
+	sysWrite = write(secondFD, buffer, sysRead);
+	if (sysWrite == -1)
+	{
+		printf("Error: Can't write to, %s\n", fileto);
+		_close(secondFD);
+		exit(99);
+	}
+	_close(firstFD);
+	_close(secondFD);
+	return (0);
+}
+
+/**
+ * main - Main entry
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(int argc, char **argv)
+{
+	int resp;
+	
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp %s %s\n", argv[1], argv[2]);
+		exit(97);
+	}
+	resp = copy_file(argv[1], argv[2]);
+	return (resp);
+}
