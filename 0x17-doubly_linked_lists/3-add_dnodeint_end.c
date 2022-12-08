cdcc6585188849fc123be2cdfa6ade8b2158b6de
George Osondu <browngeorge752@gmail.com>
2022-12-08 21:58:16 +0000

    add elements end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,7 +28,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new);
 	}
 	current = *head;
-	while (!current->next)
+	while (current->next)
 		current = current->next;
 	current->next = new;
 	new->prev = current;
