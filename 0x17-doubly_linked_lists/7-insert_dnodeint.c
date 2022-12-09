ac401852e43a9c01e4e04d4fdb38d6d6d4bc3396
George Osondu <browngeorge752@gmail.com>
2022-12-09 03:19:17 +0000

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,11 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp, *current, *new = NULL;
 	unsigned int i = 1;
 
-	if (!*h)
-		return (NULL);
-	if (!*h && idx != 0)
-		return (NULL);
-
 	current = *h;
 	if (idx == 0)
 	{
