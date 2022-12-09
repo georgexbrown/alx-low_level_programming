5f958290e9c793b53de036be93e3621a81bc1e82
George Osondu <browngeorge752@gmail.com>
2022-12-09 03:03:35 +0000

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *current, *new;
+	dlistint_t *tmp, *current, *new = NULL;
 	unsigned int i = 1;
 
 	if (!*h)
@@ -49,4 +49,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 		current = current->next;
 	}
+	return (new);
 }
