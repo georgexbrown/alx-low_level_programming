7e53ee29520439cb20da8b506f74d9f9c1178029
George Osondu <browngeorge752@gmail.com>
2023-01-07 02:19:50 +0000

    create table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly createed hash table or NULL if not successful
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	/* creates a new hash table */
+	hash_table_t newTable;
+	unsigned int i = 0;
+
+	newTable = malloc(sizeof(hash_table_t));
+	if (!newTable)
+		return (NULL);
+	
+	newTable->size = size;
+	newTable->array = malloc(sizeof(hash_node_t*) * newTable->size);
+	if (!newTable->array)
+		return (NULL);
+
+	for (; i < newTable->size; i++)
+		newTable->array[i] = NULL;
+
+	return (newTable);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,19 @@
+Hash Table Data Structure in C Programming
+
+--------------------------
+
+Understanding the function of Hashing in C
+
+0 - a function that creates a hash table.
+
+1 - a hash function implementing the djb2 algorithm.
+
+2 - a function that gives you the index of a key.
+
+3 - a function that adds an element to the hash table.
+
+4 - a function that retrieves a value associated with a key.
+
+5 - a function that prints a hash table.
+
+6 - a function that deletes a hash table.
